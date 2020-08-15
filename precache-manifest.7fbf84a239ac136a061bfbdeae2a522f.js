85a97217e5b5a74358b2659abfddeda5d5a01fd5
BhutadaRohan <gopalbhutada2221@gmail.com>
2020-08-15 12:05:12 +0530

    Updates

---
+++ precache-manifest.7fbf84a239ac136a061bfbdeae2a522f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "592ce8a4acec8846aae937010292b909",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "c344eb58942b2fe626b1",
+    "url": "/corona-tracker/static/css/main.ed82b16f.chunk.css"
+  },
+  {
+    "revision": "4ebbb1752d5f519a7d0c",
+    "url": "/corona-tracker/static/js/2.b5a895e6.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-tracker/static/js/2.b5a895e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c344eb58942b2fe626b1",
+    "url": "/corona-tracker/static/js/main.bb67571b.chunk.js"
+  },
+  {
+    "revision": "a8fd732192653b20bcfa",
+    "url": "/corona-tracker/static/js/runtime-main.b5ea7f81.js"
+  }
+]);
